333fc55a9d7a29b6176837c579b8bf96da7ee290
StudentAlleg <studentalleg-pilot@yahoo.com>
2023-05-17 20:01:58 -0700

    abstracting cannon

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -6,20 +6,30 @@ class GameInterface extends Phaser.Scene {
 
     create() {
         //first, lets make the cannon
-        this.cannon = this.add.container(50, this.cameras.main.height - 50);
-        let cannonColor = 0xFF0000;
+        this.cannon = this.newCannon(50, this.cameras.main.height - 50, 0xFFFFFF);
 
-        this.base = this.add.circle(0, 0, 100, cannonColor);
-        this.cannon.add(this.base);
-
-        this.barrel = this.add.rectangle(0, 0, 50, 300, cannonColor);
-        this.barrel.setAngle(45);
-        this.cannon.add(this.barrel);
-
+        this.
 
         //cannon
         this.onEnter();
     }
+
+
+    newCannon(x, y, color) {
+        //first, lets make the cannon
+        cannon = this.add.container(x, y);
+
+        base = this.add.circle(0, 0, 100, color);
+        cannon.add(this.base);
+
+        barrel = this.add.rectangle(0, 0, 50, 300, color);
+        barrel.setAngle(45);
+        
+        cannon.add(this.barrel);
+        return cannon;
+    }
+
+
 
     onEnter() {
         //For consumer classes
