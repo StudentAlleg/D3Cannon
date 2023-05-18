984c1f8bb8892bde14fd631008aad0cde89ff63f
StudentAlleg <studentalleg-pilot@yahoo.com>
2023-05-18 00:14:46 -0700

    basic code for the cannon ball

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -8,7 +8,7 @@ class GameInterface extends Phaser.Scene {
         //first, lets make the cannon
         this.cannon = this.newCannon(50, this.cameras.main.height - 50, 0xFFFFFF);
 
-        this.
+        
 
         //cannon
         this.onEnter();
@@ -20,13 +20,47 @@ class GameInterface extends Phaser.Scene {
         cannon = this.add.container(x, y);
 
         base = this.add.circle(0, 0, 100, color);
+        barrel.setName("base");
         cannon.add(this.base);
 
         barrel = this.add.rectangle(0, 0, 50, 300, color);
         barrel.setAngle(45);
+        barrel.setName("barrel");
         
         cannon.add(this.barrel);
         return cannon;
+    }
+
+    fireCannon(cannon, power, color) {
+        let ball = this.newBall(cannon, power, color);
+        //do other cannon firing things
+        return ball;
+    }
+
+    newBall(cannon, power, color) {
+        //make a new ball at the end of the barrel
+        //get the barrel, then change where the x,y is from
+        let barrel = cannon.getByName("barrel");
+        barrel.setOrigin(1, 0.5);
+        
+        //get the x, y of the barrel
+        let x = cannon.x + barrel.x;
+        let y = cannon.y + barrel.y;
+        
+        barrel.setOrigin(0.5, 0.5);
+
+
+        let ball = this.add.circle(x, y, 25, color);
+        ball = this.physics.existing(ball);
+        ball.body.allowGravity(true);
+
+        let rotation = barrel.rotation;
+        let vX = Math.cos(rotation) * power;
+        let vY = Math.sin(rotation) * power;
+        ball.body.setVelocity(vX, vY);
+        return ball;
+
+
     }
 
 
