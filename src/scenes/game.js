ee21f4f42bf7f570b38a884944ea9c6c18930b10
StudentAlleg <studentalleg-pilot@yahoo.com>
2023-05-19 01:29:29 -0700

    ironing out firing mechanism

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -8,19 +8,29 @@ class GameInterface extends Phaser.Scene {
         //first, lets make the cannon
         let color = 0xFFFFFF;
         this.power = 0;
+        this.powerButton = false; //is the power button being pressed?
         
         this.cannon = this.newCannon(50, this.cameras.main.height - 50, color);
 
+        let fireDown = () => {
+            while(this.input.activePointer.leftButtonDown()) {
+                this.power += 50
+                console.log
+            } ;
+        }
+        let fireUp = () => {
+            this.fireCannon(this.cannon, this.power, 0x00FF00);
+            this.power = 0;
+        }
 
 
-        let testBall = () => this.newBall(this.cannon, 2000, 0x00FF00);
         let increaseAngle = () => this.changeAngle(this.cannon, 1, 90, 0);
         let decreaseAngle = () => this.changeAngle(this.cannon, -1, 90, 0)
 
         this.down = this.newButton(500, this.cameras.main.height - 50, "Down", 0x444444, increaseAngle);
         this.up = this.newButton(500, this.cameras.main.height - 125 - this.down.height, "Up", 0x444444, decreaseAngle);
         
-        this.testButton = this.newButton(700, this.cameras.main.height - 50, "Charge", 0xFF0000, testBall);
+        this.testButton = this.newButton(700, this.cameras.main.height - 50, "Charge", 0xFF0000, fireDown, fireUp);
 
         //cannon
         this.onEnter();
@@ -75,14 +85,18 @@ class GameInterface extends Phaser.Scene {
         return ball;
     }
 
-    newButton(x, y, text, color, fn) {
+    newButton(x, y, text, color, downFn, upFn = undefined) {
         let button = this.add.container(x,y);
         let padding = 5;
         let textObj = this.add.text(0, 0, text)
             .setFontSize(56)
             .setOrigin(0.5, 0.5)
             .setInteractive()
-            .on('pointerdown', fn);
+            .on('pointerdown', downFn);
+
+        if (upFn != undefined) {
+            textObj.on('pointerup', upFn);
+        }
         
         let background = this.add.rectangle(0, 0, textObj.width + padding, textObj.height + padding, color);
            
@@ -107,6 +121,13 @@ class GameInterface extends Phaser.Scene {
         barrel.setAngle(angle);
     }
 
+    update() {
+
+    }
+
+    onUpdate() {
+        //For consumer classes
+    }
     onEnter() {
         //For consumer classes
     }
